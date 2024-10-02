5e87ec3d7933b57ed938f2ed4ce336cc242762d1
pendrive <junhaumhayden@hotmail.com>
2024-10-02 19:59:37 -0300

    second commit

--- src/main/java/tst/firstStep/firstStepOnRest/NumberOneApplication.java
+++ src/main/java/tst/firstStep/firstStepOnRest/NumberOneApplication.java
@@ -1,7 +1,5 @@
 package tst.firstStep.firstStepOnRest;
 
-import javax.swing.Spring;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
